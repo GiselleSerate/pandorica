5ef9e3bf3c190177f4807acad02d60a0f81e1471
GiselleSerate <giselle.serate@gmail.com>
2019-05-22 12:15:34 -0700

    Comment search file

--- searcher.py
+++ searcher.py
@@ -1,10 +1,13 @@
 # Basic search of elasticsearch db
 from elasticsearch_dsl import Search, connections
 
+# Establish connection (localhost:9200 by default, which works for us)
 connections.create_connection()
 
+# Create search of sample web data looking for deb extensions
 eventSearch = Search(index='kibana_sample_data_logs').query('match', extension='deb')
 eventSearch.execute()
 
+# Print IPs of all matching documents
 for hit in eventSearch:
     print(f"{hit['ip']}")
