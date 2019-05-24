8f765da4c1d6828cd23a0f92c73d26dc8a60e15b
GiselleSerate <giselle.serate@gmail.com>
2019-05-24 10:41:28 -0700

    Implement remove and comment

--- parser.py
+++ parser.py
@@ -1,18 +1,39 @@
 from bs4 import BeautifulSoup
-import re # for regex
+import re
 
+added = []
+removed = []
+
+# Open and parse file
 data = open('./updates.html')
 soup = BeautifulSoup(data, 'html5lib')
 
+# Define regex so we can search for tags beginning with this
 addedPattern = re.compile(r'New Spyware DNS C2 Signatures')
 removedPattern = re.compile(r'Old Spyware DNS C2 Signatures')
 
-addedHeader = soup.find('h3', text=addedPattern)
-print(addedHeader)
-addedTable = addedHeader.find_next_sibling('table')
-tds = addedTable.find_all('td')
-for td in tds:
-    print(td.string)
 
-# removedHeader = soup.find('h3', text=removedPattern)
-# print(removedHeader)
+# Added: Pull out a list of tds from parse tree
+header = soup.find('h3', text=addedPattern)
+table = header.find_next_sibling('table')
+tds = table.find_all('td')
+
+# Added: Get domains from table entries
+for td in tds:
+    rawDomain = td.string
+    added.append(rawDomain.split(':')[1])
+
+
+# Removed: Pull out a list of tds from parse tree
+header = soup.find('h3', text=removedPattern)
+table = header.find_next_sibling('table')
+tds = table.find_all('td')
+
+# Removed: Get domains from table entries
+for td in tds:
+    rawDomain = td.string
+    removed.append(rawDomain.split(':')[1][:-1]) # Removed has an extraneous close parenthesis
+
+# Print summary of parse
+print(f'{len(added)} domains added, like {added[:3]}')
+print(f'{len(removed)} domains removed, like {removed[:3]}')
