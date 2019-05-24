0c65f647fc4e86aeba62c8dcbfe5d927d2655197
GiselleSerate <giselle.serate@gmail.com>
2019-05-24 10:22:44 -0700

    Refactor for less nesting

--- parser.py
+++ parser.py
@@ -10,13 +10,9 @@ removedPattern = re.compile(r'Old Spyware DNS C2 Signatures')
 addedHeader = soup.find('h3', text=addedPattern)
 print(addedHeader)
 addedTable = addedHeader.find_next_sibling('table')
-for addedTableBody in addedTable.children: # tablebody level
-    for addedItem in addedTableBody: 
-        # If the element is a tag rather than a navigable string
-        if not isinstance(addedItem, str): # tr level
-            tds = addedItem.find_all('td')
-            for td in tds:
-                print(td.string)
+tds = addedTable.find_all('td')
+for td in tds:
+    print(td.string)
 
 # removedHeader = soup.find('h3', text=removedPattern)
 # print(removedHeader)
