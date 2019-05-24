07177857da05fe955118e0c92a3b0fe7c7c7b5b5
GiselleSerate <giselle.serate@gmail.com>
2019-05-24 10:21:39 -0700

    Print all added urls as strings

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import re # for regex
 
-soup = BeautifulSoup(open("./updates.html"), "html.parser")
+data = open('./updates.html')
+soup = BeautifulSoup(data, 'html5lib')
 
 addedPattern = re.compile(r'New Spyware DNS C2 Signatures')
 removedPattern = re.compile(r'Old Spyware DNS C2 Signatures')
@@ -9,11 +10,13 @@ removedPattern = re.compile(r'Old Spyware DNS C2 Signatures')
 addedHeader = soup.find('h3', text=addedPattern)
 print(addedHeader)
 addedTable = addedHeader.find_next_sibling('table')
-for addedTableBody in addedTable.children: # TODO fix how much whitespace there is in here. wow
-    print("foo")
+for addedTableBody in addedTable.children: # tablebody level
     for addedItem in addedTableBody: 
-      print("item:")
-      print(addedItem)
+        # If the element is a tag rather than a navigable string
+        if not isinstance(addedItem, str): # tr level
+            tds = addedItem.find_all('td')
+            for td in tds:
+                print(td.string)
 
 # removedHeader = soup.find('h3', text=removedPattern)
 # print(removedHeader)
