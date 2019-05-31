0124fba1d778d496a3e09d4a4e95221142afd353
GiselleSerate <giselle.serate@gmail.com>
2019-05-31 13:40:10 -0700

    Change script to scrape from actual support portal

--- parser.py
+++ parser.py
@@ -9,7 +9,6 @@ from elasticsearch_dsl import DocType, Keyword, Text, connections
 from flask import Flask
 import urllib.request
 
-import config
 import sys # TODO: only for the next line
 sys.path.append('../content_downloader') # TODO: this is Bad and I'm Sorry.
 import content_downloader
@@ -117,10 +116,14 @@ if __name__ == '__main__':
     # Domains get stored here
     added = []
     removed = []
+    
 
+    username = app.config['USERNAME']
+    password = app.config['PASSWORD']
+    # username, password, download_dir = content_downloader.get_config('../content_downloader/content_downloader.conf')
 
     # Create contentdownloader object to get AV release notes
-    content_downloader = content_downloader.ContentDownloader(username=app.config['USERNAME'], password=app.config['PASSWORD'], package='antivirus',
+    content_downloader = content_downloader.ContentDownloader(username=username, password=password, package='antivirus',
                                            debug=False, isReleaseNotes=True)
 
     # Check latest version. Login if necessary.
@@ -129,37 +132,15 @@ if __name__ == '__main__':
     # Determine latest update
     filename, foldername, latestversion = content_downloader.find_latest_update(updates)
 
-    # TODO: the following checks if we've done this before, but we don't care atm
-    # # Get previously downloaded versions from download directory
-    # downloaded_versions = []
-    # for f in os.listdir(download_dir):
-    #     downloaded_versions.append(f)
-
-    # # Check if already downloaded latest and do nothing
-    # if filename in downloaded_versions:
-    #     logging.debug("Already downloaded latest version: {0}".format(filename))
-    #     sys.exit(0)
-
     # Get download URL
     fileurl = content_downloader.get_download_link(token, filename, foldername)
 
     # Get HTML file to parse
     try:
         data = urllib.request.urlopen(fileurl)
-        # data = open('./updates.html') # TODO: uncomment if you don't want to worry about hosting
     except urllib.error.URLError:
         print(f'Updates failed to download from {fileurl}')
         raise SystemExit
-
-    # TODO: I'm downloading it a different way
-    # # Download latest version to download directory
-    # logging.debug("Downloading latest version: %s" % latestversion)
-    # filename = content_downloader.download(download_dir, fileurl, filename)
-    # if filename is not None:
-    #     print("Finished downloading file: %s" % filename)
-    # else:
-    #     print("Unable to download latest content update")
-    #     raise SystemExit
 
 
     # Parse file
