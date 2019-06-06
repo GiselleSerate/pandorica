4fdc97f665ee9fc6112ee2d76ccfb815139bf2c3
GiselleSerate <giselle.serate@gmail.com>
2019-06-06 15:41:26 -0700

    Call safe networking function

--- ask_autofocus.py
+++ ask_autofocus.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import time
 
+from elasticsearch_dsl import connections
 from flask import Flask
 import requests
 from logging.config import dictConfig
@@ -11,4 +12,11 @@ sys.path.append('../safe-networking') # TODO: this is Bad and I'm Sorry.
 from project.dns.dnsutils import getDomainInfo
 
 
+# Establish database connection (port 9200 by default)
+connections.create_connection(host='10.54.92.75')
+
 print(getDomainInfo('www.ldlvchurch.com'))
+
+
+# Get information about the tag
+# /tag/{public_tag_name}
