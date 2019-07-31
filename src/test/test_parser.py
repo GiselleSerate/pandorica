2aee910cecf0658718824ebd038eaaad86694d27
GiselleSerate <giselle.serate@gmail.com>
2019-07-30 17:41:57 -0700

    Add lineno to logs

--- src/test/test_parser.py
+++ src/test/test_parser.py
@@ -110,7 +110,7 @@ def test_all():
     dictConfig({
         'version': 1,
         'formatters': {'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+            'format': '[%(asctime)s] %(levelname)s in %(module)s(%(lineno)s): %(message)s',
         }},
         'handlers': {'wsgi': {
             'class': 'logging.StreamHandler',
