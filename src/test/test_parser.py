d5b3fb87ce0b701789d818df3ea8fb66dd409ea1
GiselleSerate <giselle.serate@gmail.com>
2019-07-31 09:02:56 -0700

    Change info back to debug

--- src/test/test_parser.py
+++ src/test/test_parser.py
@@ -176,7 +176,7 @@ def test_all():
             dom_search.execute()
 
             for hit in dom_search:
-                logging.info(hit)
+                logging.debug(hit)
                 present = True
                 for key, value in case.items():
                     # Generic domains have no threat name; will give key error.
